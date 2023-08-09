cc6392401dd9fb6e45b1e3f253a43b08ec4fa364
Hamza Rehmani <91487023+hamzarehmani109@users.noreply.github.com>
2023-08-08 17:09:15 -0700

    first commit

---
+++ Shoes-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Herosection from "./components/Hero";
+const App = () => {
+    return (
+        <div>
+    <Navigation/> 
+    <Herosection/>
+    </div>
+    );
+  };
+  
+  export default App;
+  
